95a6bcd3cededf0c9ff23dbe8f01c3e7dd9ea09f
latblackmon <latwoineblackmon8@gmail.com>
2022-10-16 14:00:44 +0700

    Update

--- src/pages/StatsPage.jsx
+++ src/pages/StatsPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Dropdown from "../components/dropdown/Dropdown";
-import Heading from "../components/layout/Heading";
+import Footer from "../components/layout/Footer";
 import PageContainer from "../components/layout/PageContainer";
 import CollectionStats from "./styles/stats/CollectionStats";
 
@@ -12,7 +11,6 @@ const StatsPageStyles = styled.div`
   background-position: top left;
   padding: 0 !important;
   width: 100%;
-  height: 4700px;
 `;
 
 const StatsPage = () => {
@@ -20,46 +18,7 @@ const StatsPage = () => {
     <StatsPageStyles>
       <PageContainer>
         <CollectionStats></CollectionStats>
-        <div className="mt-[45px] font-[600] text-[14px]">
-          <div className="header flex opacity-70">
-            <div className="flex-1 flex items-center justify-center">Rank</div>
-            <div className="flex-[3]">Collection</div>
-            <div className="flex-1 flex items-center justify-center">
-              Volume
-            </div>
-            <div className="flex-1 flex items-center justify-center">
-              % Change
-            </div>
-            <div className="flex-1 flex items-center justify-center">
-              Floor Price
-            </div>
-            <div className="flex-1 flex items-center justify-center">Sales</div>
-          </div>
-          <div className="body flex mt-10 rounded-lg items-center justify-center bg-[#1D1922] h-20">
-            <div className="flex-1 flex items-center justify-center">1</div>
-            <div className="flex-[3] flex items-center">
-              <img
-                src=""
-                alt=""
-                className="w-[60px] h-[60px] object-cover rounded-[4px] mr-9"
-              />
-              <span className="mr-[10px]">Blood bats</span>
-              <img src="" alt="" className="w-4 h-4 object-cover" />
-            </div>
-            <div className="flex-1 flex items-center justify-center">
-              <span className="">691 ETH</span>
-            </div>
-            <div className="flex-1 flex items-center justify-center">
-              <span className=" text-[#1CFD76]">+124%</span>
-            </div>
-            <div className="flex-1 flex items-center justify-center text-[#FF910F]">
-              15.10 ETH
-            </div>
-            <div className="flex-1 flex items-center justify-center text-[#c88efe]">
-              43
-            </div>
-          </div>
-        </div>
+        <Footer></Footer>
       </PageContainer>
     </StatsPageStyles>
   );
--- src/pages/styles/stats/CollectionStats.jsx
+++ src/pages/styles/stats/CollectionStats.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Dropdown from "../../../components/dropdown/Dropdown";
 import Heading from "../../../components/layout/Heading";
+import Blood_bat from "../../../assets/collection/Blood_bat.png";
+import verify from "../../../assets/outside/verify.png";
 
 const categories = [
   { id: 1, name: "All categories" },
@@ -27,6 +29,44 @@ const CollectionStats = () => {
       <div className="mt-[25px] flex gap-x-10">
         <Dropdown className="w-[180px]" listArr={categories}></Dropdown>
         <Dropdown className="w-[150px]" listArr={chains}></Dropdown>
+      </div>
+      <div className="mt-[45px] font-[600] text-[14px]">
+        <div className="header flex opacity-70">
+          <div className="flex-1 flex items-center justify-center">Rank</div>
+          <div className="flex-[3]">Collection</div>
+          <div className="flex-1 flex items-center justify-center">Volume</div>
+          <div className="flex-1 flex items-center justify-center">
+            % Change
+          </div>
+          <div className="flex-1 flex items-center justify-center">
+            Floor Price
+          </div>
+          <div className="flex-1 flex items-center justify-center">Sales</div>
+        </div>
+        <div className="body flex mt-10 rounded-lg items-center justify-center bg-[#1D1922] h-20">
+          <div className="flex-1 flex items-center justify-center">1</div>
+          <div className="flex-[3] flex items-center">
+            <img
+              src={Blood_bat}
+              alt=""
+              className="w-[60px] h-[60px] object-cover rounded-[4px] mr-9"
+            />
+            <span className="mr-[10px]">Blood bats</span>
+            <img src={verify} alt="" className="w-4 h-4 object-cover" />
+          </div>
+          <div className="flex-1 flex items-center justify-center">
+            <span className="">691 ETH</span>
+          </div>
+          <div className="flex-1 flex items-center justify-center">
+            <span className=" text-[#1CFD76]">+124%</span>
+          </div>
+          <div className="flex-1 flex items-center justify-center text-[#FF910F]">
+            15.10 ETH
+          </div>
+          <div className="flex-1 flex items-center justify-center text-[#c88efe]">
+            43
+          </div>
+        </div>
       </div>
     </>
   );
