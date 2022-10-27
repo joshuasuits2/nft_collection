01079835d9261c40b26c14e73f89f2624d3ef4a4
latblackmon <latwoineblackmon8@gmail.com>
2022-10-27 15:12:19 +0700

    update navigation

---
+++ src/components/slider/SliderCustom.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Splide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import styled from "styled-components";
+
+const SliderCustomStyles = styled.div`
+  .splide__arrow {
+    opacity: 1;
+    width: 53px;
+    height: 53px;
+    background: linear-gradient(
+      270deg,
+      rgba(180, 68, 255, 0) -70.75%,
+      rgba(245, 130, 255, 0.9) 130.19%
+    );
+  }
+  .splide__arrow.your-class-arrow {
+    svg {
+      fill: white;
+    }
+  }
+  .splide__arrow--prev.your-class-prev {
+    left: -71px;
+  }
+  .splide__arrow--next.your-class-next {
+    right: -71px;
+  }
+  .splide__pagination {
+    bottom: -15%;
+    display: flex;
+    gap: 3px;
+  }
+  .splide__pagination__page.is-active {
+    opacity: 1;
+    background-image: linear-gradient(
+      180deg,
+      rgb(172, 71, 255),
+      rgb(193, 152, 251)
+    );
+  }
+`;
+
+const SliderCustom = ({ children }) => {
+  return (
+    <SliderCustomStyles>
+      <Splide
+        options={{
+          type: "loop",
+          perPage: 4,
+          focus: "left",
+          gap: "50px",
+          autoScroll: {
+            speed: 2,
+          },
+          classes: {
+            arrows: "splide__arrows your-class-arrows",
+            arrow: "splide__arrow your-class-arrow",
+            prev: "splide__arrow--prev your-class-prev",
+            next: "splide__arrow--next your-class-next",
+            pagination: "splide__pagination your-class-pagination",
+            page: "splide__pagination__page your-class-page",
+          },
+        }}
+      >
+        {children}
+      </Splide>
+    </SliderCustomStyles>
+  );
+};
+
+export default SliderCustom;
---
+++ src/fakeAPI/Categories.js
@@ -0,0 +1,64 @@
+import MeeCat602 from "../assets/nfts/MeeCat602.png";
+import MeeCat608 from "../assets/nfts/MeeCat608.png";
+import LovelyWork from "../assets/nfts/lovely_work_608.png";
+import MeeCat6902 from "../assets/nfts/MeeCat6902.png";
+import MeeCat523 from "../assets/nfts/MeeCat523.png";
+import Ethereum from "../assets/outside/ethereum.png";
+
+export const ListCategory = [
+  {
+    id: 1,
+    img: MeeCat602,
+    name: "MeeCat #602",
+    price: "0.03 ETH",
+    coin: Ethereum,
+    creator: "Layer Lab",
+    owner: "Layer Lab",
+    collection: "MeeCat",
+    auction: [2, 5, 1, 6, 3, 3],
+  },
+  {
+    id: 2,
+    img: LovelyWork,
+    name: "Lovely Work #586",
+    price: "0.05 ETH",
+    coin: Ethereum,
+    creator: "Yurii",
+    owner: "Henry",
+    collection: "Lovely Work",
+    auction: [0, 2, 2, 2, 3, 7],
+  },
+  {
+    id: 3,
+    img: MeeCat608,
+    name: "MeeCat #608",
+    price: "0.03 ETH",
+    coin: Ethereum,
+    creator: "Layer Lab",
+    owner: "Layer Lab",
+    collection: "Lovely Work",
+    auction: [7, 2, 1, 8, 2, 2],
+  },
+  {
+    id: 4,
+    img: MeeCat6902,
+    name: "MeeCat #6902",
+    price: "0.04 ETH",
+    coin: Ethereum,
+    creator: "Layer Lab",
+    owner: "Henry",
+    collection: "Lovely Work",
+    auction: [2, 6, 0, 9, 5, 2],
+  },
+  {
+    id: 5,
+    img: MeeCat523,
+    name: "MeeCat #523",
+    price: "0.05 ETH",
+    coin: Ethereum,
+    creator: "Layer Lab",
+    owner: "Henry",
+    collection: "Lovely Work",
+    auction: [4, 2, 1, 0, 2, 5],
+  },
+];
