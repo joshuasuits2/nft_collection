41300cf3427795fef9239963855c16b29926013a
latblackmon <latwoineblackmon8@gmail.com>
2022-12-14 15:45:23 +0700

    Update UI UX

---
+++ src/components/form/FormSign.js
@@ -0,0 +1,151 @@
+import axios from "axios";
+import React, { useState } from "react";
+import AuthUser from "../../config/AuthUser";
+import { baseURL } from "../../config/getConfig";
+import useAccountBalance from "../../hooks/useAccountBalance";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const FormSign = ({ nftInfoDetail, handleSetConfirmBtn = () => {} }) => {
+  const [signBtn, setSignBtn] = useState(false);
+  const { token } = AuthUser;
+  const { accountBalance, setAccountBalance, userId } = useAccountBalance();
+  let remainBalance = (
+    accountBalance?.balance - parseFloat(nftInfoDetail?.price)
+  ).toFixed(4);
+  console.log(typeof parseFloat(remainBalance));
+
+  const handleComplete = async () => {
+    await axios
+      .post(
+        `${baseURL}/api/account_balances/${accountBalance.id}`,
+        {
+          balance: parseFloat(remainBalance),
+        },
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        toast.success("Successfully!");
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div>
+      {signBtn === false ? (
+        <div className="flex flex-col transition-all duration-500">
+          <span
+            className="top-5 left-5 absolute cursor-pointer"
+            onClick={handleSetConfirmBtn}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="currentColor"
+              className="bi bi-chevron-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+              />
+            </svg>
+          </span>
+          <span className="font-[700] tracking-normal text-[18px] text-center">
+            Complete Checkout
+          </span>
+          <div className="h-[1px] mt-3 w-full bg-gray-200"></div>
+          <div className="mt-5 text-[16px] flex flex-col text-left">
+            <span>To complete your purchase, follow these step:</span>
+            <div className="flex gap-x-4 items-center mt-5 w-full font-[600] text-[16px] px-5 py-4 border border-solid  border-gray-200">
+              <div className="w-8 h-8 border-4 border-solid border-gray-900 rounded-full "></div>
+              <span>Sign message</span>
+            </div>
+            <div className="w-full font-[600] text-[14px] p-5 border border-solid  border-gray-200 text-gray-500 border-t-transparent ">
+              <span>Sign a message using your wallet to continue</span>
+            </div>
+          </div>
+          <button
+            onClick={() => setSignBtn(true)}
+            className="mt-5 inline-flex items-center justify-center px-8 py-4 font-sans font-semibold tracking-wide text-white bg-[#c084fc] rounded-lg mx-auto w-[300px] [h-[53px] active:bg-purple-300"
+          >
+            Sign
+          </button>
+        </div>
+      ) : (
+        <div className="flex flex-col transition-all duration-500">
+          <span
+            className="top-5 left-5 absolute cursor-pointer"
+            onClick={() => setSignBtn(false)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="currentColor"
+              className="bi bi-chevron-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+              />
+            </svg>
+          </span>
+          <span className="font-[700] tracking-normal text-[18px] text-center">
+            Complete Checkout
+          </span>
+          <div className="h-[1px] mt-3 w-full bg-gray-200"></div>
+          <div className="flex flex-col items-end justify-center py-3 border-b-[1px] border-dashed">
+            <div className="w-full flex justify-between items-center">
+              <span className="font-[600] text-[18px]">My balance:</span>
+              <span className="font-[600] text-[20px] text-[#5f6368]">
+                {accountBalance?.balance} ETH
+              </span>
+            </div>
+
+            <div className="mt-5 flex w-full justify-between">
+              <div className="flex gap-3 text-[15px] tracking-normal">
+                <div className="w-[70px] h-[70px]">
+                  <img
+                    src={`${baseURL}/storage/nftImages/${nftInfoDetail?.url_image_nft}`}
+                    alt=""
+                    className="w-full h-full object-cover rounded-lg"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <span className="font-[600] text-gradient">
+                    Collection: {nftInfoDetail.collection.name}
+                  </span>
+                  <span className="mt-2">
+                    <span className="font-[600]"> Owner:</span>{" "}
+                    {nftInfoDetail.owner.name}
+                  </span>
+                </div>
+              </div>
+              <span className="text-end">{nftInfoDetail.price} ETH</span>
+            </div>
+          </div>
+          <span className="mt-5 text-end">{remainBalance} ETH</span>
+
+          <button
+            onClick={handleComplete}
+            className="mt-5 inline-flex items-center justify-center px-8 py-4 font-sans font-semibold tracking-wide text-white bg-[#c084fc] rounded-lg mx-auto w-[300px] [h-[53px] active:bg-purple-300"
+          >
+            Complete
+          </button>
+        </div>
+      )}
+      <ToastContainer></ToastContainer>
+    </div>
+  );
+};
+
+export default FormSign;
