bc3b4df05e943db881c716aceafdb3564c49b0f8
latblackmon <latwoineblackmon8@gmail.com>
2022-12-23 22:53:23 +0700

    Update Code

--- src/components/admin/admin-page/Topics.jsx
+++ src/components/admin/admin-page/Topics.jsx
@@ -3,10 +3,40 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import DeleteIcon from "../../../assets/icons/DeleteIcon";
 import EditIcon from "../../../assets/icons/EditIcon";
+import AuthUser from "../../../config/AuthUser";
 import { baseURL } from "../../../config/getConfig";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Topics = () => {
   const [topics, setTopics] = useState([]);
+  const [addTopics, setAddTopics] = useState(false);
+  const [editTopics, setEdditTopics] = useState(false);
+  const { token } = AuthUser();
+  const [textTopic, setTextTopic] = useState();
+
+  const handleAddNewTopic = async () => {
+    try {
+      await axios.post(
+        `${baseURL}/api/topics`,
+        {
+          name: textTopic,
+          image_url: "No image",
+        },
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      toast.success("Update successful!");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     (async () => {
       try {
@@ -66,15 +96,56 @@ const Topics = () => {
                     </tr>
                   </tbody>
                 ))}
+              {addTopics === true && (
+                <tbody>
+                  <tr>
+                    <th className="font-[400] py-3">
+                      {topics?.length + 1 || ""}
+                    </th>
+                    <th className="font-[400] py-3">
+                      <input
+                        type="text"
+                        placeholder=""
+                        onChange={(e) => setTextTopic(e.target.value)}
+                        className="px-5 py-3 border border-slate-600 border-solid rounded-[4px] outline-none bg-transparent text-center"
+                      />
+                    </th>
+                  </tr>
+                </tbody>
+              )}
             </table>
-            <div className="w-full ml-auto max-w-[200px]">
-              <button className="top-[10%] right-0 mt-5 w-full inline-flex items-center justify-center px-8 py-4 font-sans font-semibold text-white bg-[#4df6a1] rounded-lg h-[53px]">
-                + Add New Item
-              </button>
-            </div>
+            {addTopics === true ? (
+              <div className="flex gap-x-6 w-[300px] ml-auto">
+                <button
+                  onClick={() => setAddTopics(false)}
+                  className="top-[10%] right-0 mt-5 w-full inline-flex items-center justify-center px-8 py-4 font-sans font-semibold text-white bg-[#ed3e50] rounded-lg h-[53px]"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={() => {
+                    handleAddNewTopic();
+                    setAddTopics(false);
+                  }}
+                  className="top-[10%] right-0 mt-5 w-full inline-flex items-center justify-center px-8 py-4 font-sans font-semibold text-white bg-[#4df6a1] rounded-lg h-[53px]"
+                >
+                  Save
+                </button>
+              </div>
+            ) : (
+              <div className="w-full ml-auto max-w-[200px]">
+                <button
+                  onClick={() => setAddTopics(true)}
+                  className="top-[10%] right-0 mt-5 w-full inline-flex items-center justify-center px-8 py-4 font-sans font-semibold text-white bg-[#4df6a1] rounded-lg h-[53px]"
+                >
+                  + Add New Item
+                </button>
+              </div>
+            )}
           </div>
         </div>
       )}
+      <ToastContainer autoClose={800} />
     </>
   );
 };
